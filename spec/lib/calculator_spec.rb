492d41c415a7db1f39dc109609a09898fc14b667
Mohammad Farhan <mohammadfarhan@MFARHAN-PAT-IN.local>
2025-04-15 16:39:57 +0530

    added test cases

---
+++ spec/lib/calculator_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'calculator'
+
+RSpec.describe Calculator do
+  let(:calculator) { Calculator.new }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number for a single number' do
+      expect(calculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum of two comma-separated numbers' do
+      expect(calculator.add('1,2')).to eq(3)
+    end
+  end
+end
