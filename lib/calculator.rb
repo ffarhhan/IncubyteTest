5fa0a2760a09ca57310d19d7498b8f1dd870e941
Mohammad Farhan <mohammadfarhan@MFARHAN-PAT-IN.local>
2025-04-15 16:36:39 +0530

    added calculator.rb

---
+++ lib/calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Calculator # rubocop:disable Style/Documentation
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    numbers.split(',').map(&:to_i).sum
+  end
+end
